8a3281885a313cdb46f8bb1ed2b9be42ec2cc16f
Schuster Phillip <schusterphillip63@gmail.com>
2025-08-20 21:40:20 +0200

    remove environments from tracking

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCZ_qQETOqyGFHbJe3IxTrZCqMIEvau-4w",
-  authDomain: "deliveryapp-9a349.firebaseapp.com",
-  projectId: "deliveryapp-9a349",
-  storageBucket: "deliveryapp-9a349.firebasestorage.app",
-  messagingSenderId: "63721656827",
-  appId: "1:63721656827:web:e5ce9b0a5416cc1033bb5e"
-};
